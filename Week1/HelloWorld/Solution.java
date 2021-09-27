3e4981f093ef35d63bbe48f5753b245c42d7556e
Nguyễn Văn Hưng <20021370@vnu.edu.vn>
2021-09-28 00:02:18 +0700

    OOP

---
+++ Week1/HelloWorld/Solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public static String printHello() {
+        String p = "HelloWorld";
+        return p;
+    }
+
+    public static void main(String []args) {
+        String s = printHello();
+        System.out.print(s);
+    }
+
+}
