4b7acfcff7d7ff669bef56f77663fbaf725733c0
Nguyễn Văn Hưng <20021370@vnu.edu.vn>
2021-10-05 15:56:27 +0700

    Revert "OOP"
    This reverts commit 9415df298c2d39089e09f74b07338fc86ab3a9e1.

--- Week4/Transaction/AccountTest.java
+++
@@ -1,8 +0,0 @@
-public class AccountTest {
-    public static void main(String []args) {
-        Account acc = new Account();
-        acc.addTransaction(2000.255,"deposit");
-        acc.addTransaction(1000,"withdraw");
-        acc.printTransaction();
-    }
-}
